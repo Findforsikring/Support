12711518687f672fa728ddf6d40117a828b8ccb6
Morten Scheel <ms@findforsikring.dk>
2017-06-23 23:11:00 +0200

    Refactored to extend the client

--- src/VehicleService.php
+++ src/VehicleService.php
@@ -14,19 +14,13 @@ use Kameli\NummerpladeApi\Client;
 /**
  * Class VehicleService
  * @package Findforsikring\Support
- * @mixin Client
  */
-class VehicleService
+class VehicleService extends Client
 {
     public function __construct(){
-        if (!env('NUMMERPLADE_API_TOKEN')){
+        if (!array_key_exists('NUMMERPLADE_API_TOKEN', $_ENV)){
             throw new \Exception("NUMMERPLADE_API_TOKEN er ikke sat i .env");
         }
-        $this->client = new Client(env('NUMMERPLADE_API_TOKEN'));
-    }
-
-    function __call($name, $arguments)
-    {
-        return call_user_func_array([$this->client, $name], $arguments);
+        parent::__construct($_ENV['NUMMERPLADE_API_TOKEN']);
     }
 }
